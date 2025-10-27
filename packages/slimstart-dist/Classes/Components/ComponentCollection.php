6c8b041eeaadb56aa5635f319bf55caec6a5d5a4
Joost <joost.ramke@weareslim.de>
2025-10-27 21:29:53 +0100

    add fluid primitives

--- packages/slimstart-dist/Classes/Components/ComponentCollection.php
+++ packages/slimstart-dist/Classes/Components/ComponentCollection.php
@@ -2,9 +2,9 @@
 
 namespace WeAreSlim\SlimstartDist\Components;
 
-use TYPO3Fluid\Fluid\Core\Component\AbstractComponentCollection;
-use TYPO3Fluid\Fluid\View\TemplatePaths;
+use Jramke\FluidPrimitives\Component\AbstractComponentCollection;
 use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
+use TYPO3Fluid\Fluid\View\TemplatePaths;
 
 final class ComponentCollection extends AbstractComponentCollection
 {
