f7c74afb8c25f873e9ceb3ae1f4e3b28cda382e9
Joost <joost.ramke@weareslim.de>
2025-11-05 13:59:01 +0100

    Prevent full reloads

--- packages/slimstart-dist/vite.config.js
+++ packages/slimstart-dist/vite.config.js
@@ -3,9 +3,46 @@ import typo3, { getDefaultAllowedOrigins } from 'vite-plugin-typo3';
 import { resolve } from 'node:path';
 import tailwindcss from '@tailwindcss/vite';
 
+function ignoreHMRUpdates(options = {}) {
+	return {
+		name: 'ignore-hmr-updates',
+		hotUpdate({ server, file }) {
+			const paths = options.paths ?? [];
+			const files = options.files ?? [];
+			const shouldbeIgnored = paths.some(path => {
+				if (typeof path === 'string') return file.includes(path);
+				if (typeof path === 'object') {
+					if (!path.path) {
+						console.error('Missing path in path object');
+						return false;
+					}
+
+					if (!file.includes(path.path)) return false;
+					if (path.excludeByExtension) {
+						const shouldbeIgnoredFile = path.excludeByExtension.some(fileType => {
+							return file.endsWith(fileType);
+						});
+						if (shouldbeIgnoredFile) return false;
+					}
+
+					file.includes(path.path);
+				}
+			});
+			if (shouldbeIgnored) return [];
+
+			const shouldbeIgnoredFile = files.some(fileType => file.endsWith(fileType));
+			if (shouldbeIgnoredFile) return [];
+		},
+	};
+}
+
 /** @type {import('vite').UserConfig} */
 const config = {
 	plugins: [
+		ignoreHMRUpdates({
+			paths: [{ path: 'src/js', excludeByExtension: ['tsx', 'css'] }],
+			files: ['js', 'ts', 'html', 'php'],
+		}),
 		typo3({
 			entrypointFile: 'src/ViteEntrypoints.json',
 		}),
